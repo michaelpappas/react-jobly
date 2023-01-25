d2cad43366de487022f02202a67e561b59a92afa
michaelpappas <pappas.michael@gmail.com>
2023-01-25 11:02:37 -0800

    troubleshooting companyDetail api request

--- src/CompanyDetails.js
+++ src/CompanyDetails.js
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import JoblyApi from "./api";
+import JobList from "./JobList";
+import Loading from "./Loading";
 
 /**
  * Display Jobs and company details from a specific company
@@ -9,9 +13,35 @@ import { useParams } from "react-router-dom";
 
 function CompanyDetails() {
   const { handle } = useParams();
+
+  const [company, setCompany] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  useEffect(fetchCompanyOnMount, []);
+
+  function fetchCompanyOnMount() {
+    async function fetchCompany() {
+      console.log("handle,", handle);
+      const response = await JoblyApi.getCompany(handle);
+      console.log("response-", response);
+      setCompany(response);
+
+    }
+    fetchCompany();
+
+    setIsLoading(false);
+  }
+
+  console.log("company-", company);
+  console.log("isloading", isLoading);
+  if (isLoading) return <Loading />;
+
   return (
     <div>
-      <h1>CompanyDetails for {handle}</h1>
+      <h1>CompanyDetails for {company.name}</h1>
+      <p>{company.description}</p>
+      {/* <JobList jobs={company.jobs} /> */}
     </div>
   );
 };
