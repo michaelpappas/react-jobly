d2f71337ba62126195f42b5e4d36267c119b20c3
michaelpappas <pappas.michael@gmail.com>
2023-01-27 11:05:00 -0800

    partial change for adding user job application

--- src/Job.js
+++ src/Job.js
@@ -1,8 +1,16 @@
+import userContext from "./userContext";
+import { useContext, useState } from "react";
+
 /**
  * Renders a job card
  * Props: job - object like { id, title, salary, equity }
  */
-function Job({job}) {
+function Job({ job, applyForJob }) {
+
+  const { user, setUser } = useContext(userContext);
+
+  // const applied =
+
   return (
     <div className="Job card mb-2">
       <div className="card-body">
@@ -10,9 +18,10 @@ function Job({job}) {
         <p>{job.companyHandle}</p>
         <p>Salary:{job.salary}</p>
         <p>Equity:{job.equity}</p>
+        <button onClick={applyForJob(job.id)} />
       </div>
     </div>
-  )
+  );
 }
 
 export default Job;
